1a836ba0182adf57425c1d3a1ca2eb637b9e42e8
Ashley-Upson <ashmisterupson@gmail.com>
2018-06-08 21:25:23 +0100

    CSRF tokens

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <form method="POST" action="{{ route('login') }}">
+                        @csrf
                         <div class="form-group">
                             <label>
                                 Email
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,17 +11,18 @@
             <div class="row">
                 <div class="col-sm-6">
                     <form method="POST" action="{{ route('register') }}">
+                        @csrf
                         <div class="form-group">
                             <label>
                                 Name
                             </label>
-                            <input type="text" name="name" class="form-control input-sm" placeholder="Name..." required />
+                            <input type="text" name="name" class="form-control input-sm" placeholder="Name..." required autofocus />
                         </div>
                         <div class="form-group">
                             <label>
                                 Email
                             </label>
-                            <input type="email" name="email" class="form-control input-sm" placeholder="Email..." required autofocus />
+                            <input type="email" name="email" class="form-control input-sm" placeholder="Email..." required />
                         </div>
                         <div class="form-group">
                             <label>
