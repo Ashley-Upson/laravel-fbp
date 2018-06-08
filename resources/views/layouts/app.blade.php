a65bebc5b615c7184d044883b95ea4cba1f2c34a
Ashley-Upson <ashmisterupson@gmail.com>
2018-06-08 08:29:24 +0100

    View change prep for design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,22 +5,130 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
+    <div class="navbar navbar-fixed-top navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand brand" href="index.php">
+                    <h2>
+                        FactorioBluePrintShare
+                    </h2>
+                </a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.php">
+                        <h4>
+                            FBP Home
+                        </h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="projects.php">
+                        <h4>
+                            Add Blueprint
+                        </h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h4>
+                            View Blueprints
+                        </h4>
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+                        Dropdown <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#">
+                                Action
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Another action
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Something else here
+                            </a>
+                        </li>
+                        <li role="separator" class="divider">
+
+                        </li>
+                        <li class="dropdown-header">
+                            Nav header
+                        </li>
+                        <li>
+                            <a href="#">
+                                Separated link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            One more separated link
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row body">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-2">
+            <div class="panel-group">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        Sidebar
+                    </div>
+                    <div class="panel-body">
+                        Main body
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Welcome to aupson.co.uk
+                </div>
+                <div class="panel-body">
+                    Main content
+                </div>
+                <div class="panel-footer">
+                    Footer
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-2"></div>
+    </div>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
